e600fd157d2f44af2a6c2691fa418a6abb5e98fb
Rosemary Liu <rliu18@gmail.com>
2022-03-01 03:18:58 +0000

    Completed function to populate data model from xhr response.

--- js/clues.js
+++ js/clues.js
@@ -1,3 +1,6 @@
+/* global data */
+/* exported data */
+
 function getClues() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://jservice.io/api/random/?count=9');
@@ -6,8 +9,20 @@ function getClues() {
     // console.log(xhr.status);
     // console.log(xhr.response);
 
+    for (var i = 0; i < xhr.response.length; i++) {
+      var clueData = {
+      };
+      clueData.question = xhr.response[i].question;
+      clueData.answer = xhr.response[i].answer;
+      clueData.points = xhr.response[i].value;
+      clueData.entryId = data.nextEntryId;
+      data.clues.push(clueData);
+      data.nextEntryId++;
+    }
+
   });
   xhr.send();
 }
 
 getClues();
+// console.log(data);
--- js/data.js
+++ js/data.js
@@ -1,1 +1,7 @@
 /* exported data */
+var data = {
+  clues: [],
+  currentlyAnswering: null,
+  nextEntryId: 1
+
+};
